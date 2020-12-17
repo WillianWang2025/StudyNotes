c4d8b1d94445e739dfe8a89aab112f961d5f3af2
Anakin <wangyouyuan@yeah.net>
2020-12-17 23:38:25 +0800

    dynamic programming

--- main/maxSumOfSublist.py
+++ main/maxSumOfSublist.py
@@ -18,10 +18,10 @@ def maxOfSum(ls):
 # 方法二  动态规划法---时间复杂度约O(n)
 """
 Kadane算法又被称为扫描法，为动态规划（dynamic programming）的一个典型应用。我们用DP来解决最大子数组和问题：对于数组a，用ci标记
-子数组a[0..i]的最大和，那么则有 ci=max{ai,ci−1+ai}
+子数组a[0..i]的最大和(以a[i]结尾),那么则有 ci=max{ai,ci−1+ai}
 子数组最大和即为maxci。
 Kadane算法比上面DP更进一步，不需要用一个数组来记录中间子数组和。通过观察容易得到：若ci−1≤0，则ci=ai。用e表示以当前为结束的子数组的最大和，
-以替代数组c；那么 e=max{ai,e+ai}
+以替代数组c；那么 e=max{Ci,e}
 """
 def max_subarray(A):
     max_ending_here = max_so_far = A[0]
